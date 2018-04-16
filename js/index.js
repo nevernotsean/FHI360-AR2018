f8d4b49384742113e533a67bf24d2b53591d9544
Sean Harrington <sean@never-not.com>
2018-04-16 02:08:58 -0400

    add images, first three slides and template WIP
    Signed-off-by: Sean Harrington <sean@never-not.com>

--- js/index.js
+++ js/index.js
@@ -7,6 +7,22 @@ import main from './site/main.js'
 
 const Site = new main()
 
+const preload = () => {
+	if (!Modernizr) return console.log('Enable Modernizer!')
+	if (!Modernizr.objectfit) {
+		$('.object-fit-container').each(function() {
+			var $container = $(this),
+				imgUrl = $container.find('img').prop('src')
+			if (imgUrl) {
+				$container
+					.css('backgroundImage', 'url(' + imgUrl + ')')
+					.addClass('compat-object-fit')
+			}
+		})
+	}
+}
+
 $(document).ready(() => {
+	preload
 	Site.init()
 })
