7b8a430eb6bbebfb08d10dea684262236ab4c398
Sean Harrington <sean@never-not.com>
2018-05-04 16:20:24 -0400

    update object fit modernizr
    Signed-off-by: Sean Harrington <sean@never-not.com>

--- js/vendor/modernizr-3.6.0.js
+++ js/vendor/modernizr-3.6.0.js
@@ -1,16 +1,16 @@
-/*! modernizr 3.5.0 (Custom Build) | MIT *
- * https://modernizr.com/download/?-cssanimations-csscolumns-customelements-flexbox-history-picture-pointerevents-postmessage-sizes-srcset-webgl-websockets-webworkers-addtest-domprefixes-hasevent-mq-prefixedcssvalue-prefixes-setclasses-testallprops-testprop-teststyles !*/
+/*! modernizr 3.6.0 (Custom Build) | MIT *
+ * https://modernizr.com/download/?-cssanimations-csscolumns-customelements-flexbox-history-objectfit-picture-pointerevents-postmessage-sizes-srcset-webgl-websockets-webworkers-addtest-domprefixes-hasevent-mq-prefixedcssvalue-prefixes-setclasses-testallprops-testprop-teststyles !*/
 !(function(e, t, n) {
 	function r(e, t) {
 		return typeof e === t
 	}
 	function o() {
 		var e, t, n, o, i, s, a
-		for (var l in C)
-			if (C.hasOwnProperty(l)) {
+		for (var l in b)
+			if (b.hasOwnProperty(l)) {
 				if (
 					((e = []),
-					(t = C[l]),
+					(t = b[l]),
 					t.name &&
 						(e.push(t.name.toLowerCase()),
 						t.options && t.options.aliases && t.options.aliases.length))
@@ -26,22 +26,22 @@
 									Modernizr[a[0]] instanceof Boolean ||
 									(Modernizr[a[0]] = new Boolean(Modernizr[a[0]])),
 							  (Modernizr[a[0]][a[1]] = o)),
-						w.push((o ? '' : 'no-') + a.join('-'))
+						C.push((o ? '' : 'no-') + a.join('-'))
 			}
 	}
 	function i(e) {
-		var t = S.className,
+		var t = T.className,
 			n = Modernizr._config.classPrefix || ''
-		if ((x && (t = t.baseVal), Modernizr._config.enableJSClass)) {
+		if ((E && (t = t.baseVal), Modernizr._config.enableJSClass)) {
 			var r = new RegExp('(^|\\s)' + n + 'no-js(\\s|$)')
 			t = t.replace(r, '$1' + n + 'js$2')
 		}
 		Modernizr._config.enableClasses &&
 			((t += ' ' + n + e.join(' ' + n)),
-			x ? (S.className.baseVal = t) : (S.className = t))
+			E ? (T.className.baseVal = t) : (T.className = t))
 	}
 	function s(e, t) {
-		if ('object' == typeof e) for (var n in e) P(e, n) && s(n, e[n])
+		if ('object' == typeof e) for (var n in e) O(e, n) && s(n, e[n])
 		else {
 			e = e.toLowerCase()
 			var r = e.split('.'),
@@ -63,13 +63,13 @@
 	function a() {
 		return 'function' != typeof t.createElement
 			? t.createElement(arguments[0])
-			: x
+			: E
 				? t.createElementNS.call(t, 'http://www.w3.org/2000/svg', arguments[0])
 				: t.createElement.apply(t, arguments)
 	}
 	function l() {
 		var e = t.body
-		return e || ((e = a(x ? 'svg' : 'body')), (e.fake = !0)), e
+		return e || ((e = a(E ? 'svg' : 'body')), (e.fake = !0)), e
 	}
 	function u(e, n, r, o) {
 		var i,
@@ -95,12 +95,12 @@
 			p.fake &&
 				((p.style.background = ''),
 				(p.style.overflow = 'hidden'),
-				(f = S.style.overflow),
-				(S.style.overflow = 'hidden'),
-				S.appendChild(p)),
+				(f = T.style.overflow),
+				(T.style.overflow = 'hidden'),
+				T.appendChild(p)),
 			(s = n(c, e)),
 			p.fake
-				? (p.parentNode.removeChild(p), (S.style.overflow = f), S.offsetHeight)
+				? (p.parentNode.removeChild(p), (T.style.overflow = f), T.offsetHeight)
 				: c.parentNode.removeChild(c),
 			!!s
 		)
@@ -110,12 +110,33 @@
 	}
 	function d(e) {
 		return e
+			.replace(/([a-z])-([a-z])/g, function(e, t, n) {
+				return t + n.toUpperCase()
+			})
+			.replace(/^-/, '')
+	}
+	function c(e, t) {
+		return function() {
+			return e.apply(t, arguments)
+		}
+	}
+	function p(e, t, n) {
+		var o
+		for (var i in e)
+			if (e[i] in t)
+				return n === !1
+					? e[i]
+					: ((o = t[e[i]]), r(o, 'function') ? c(o, n || t) : o)
+		return !1
+	}
+	function m(e) {
+		return e
 			.replace(/([A-Z])/g, function(e, t) {
 				return '-' + t.toLowerCase()
 			})
 			.replace(/^ms-/, '-ms-')
 	}
-	function c(t, n, r) {
+	function v(t, n, r) {
 		var o
 		if ('getComputedStyle' in e) {
 			o = getComputedStyle.call(e, t, n)
@@ -131,89 +152,69 @@
 		} else o = !n && t.currentStyle && t.currentStyle[r]
 		return o
 	}
-	function p(t, r) {
+	function h(t, r) {
 		var o = t.length
 		if ('CSS' in e && 'supports' in e.CSS) {
-			for (; o--; ) if (e.CSS.supports(d(t[o]), r)) return !0
+			for (; o--; ) if (e.CSS.supports(m(t[o]), r)) return !0
 			return !1
 		}
 		if ('CSSSupportsRule' in e) {
-			for (var i = []; o--; ) i.push('(' + d(t[o]) + ':' + r + ')')
+			for (var i = []; o--; ) i.push('(' + m(t[o]) + ':' + r + ')')
 			return (
 				(i = i.join(' or ')),
 				u(
 					'@supports (' + i + ') { #modernizr { position: absolute; } }',
 					function(e) {
-						return 'absolute' == c(e, null, 'position')
+						return 'absolute' == v(e, null, 'position')
 					}
 				)
 			)
 		}
 		return n
 	}
-	function m(e) {
-		return e
-			.replace(/([a-z])-([a-z])/g, function(e, t, n) {
-				return t + n.toUpperCase()
-			})
-			.replace(/^-/, '')
-	}
-	function h(e, t, o, i) {
+	function A(e, t, o, i) {
 		function s() {
-			u && (delete N.style, delete N.modElem)
+			u && (delete M.style, delete M.modElem)
 		}
 		if (((i = r(i, 'undefined') ? !1 : i), !r(o, 'undefined'))) {
-			var l = p(e, o)
+			var l = h(e, o)
 			if (!r(l, 'undefined')) return l
 		}
 		for (
-			var u, d, c, h, v, A = ['modernizr', 'tspan', 'samp'];
-			!N.style && A.length;
+			var u, c, p, m, v, A = ['modernizr', 'tspan', 'samp'];
+			!M.style && A.length;
 
 		)
-			(u = !0), (N.modElem = a(A.shift())), (N.style = N.modElem.style)
-		for (c = e.length, d = 0; c > d; d++)
+			(u = !0), (M.modElem = a(A.shift())), (M.style = M.modElem.style)
+		for (p = e.length, c = 0; p > c; c++)
 			if (
-				((h = e[d]),
-				(v = N.style[h]),
-				f(h, '-') && (h = m(h)),
-				N.style[h] !== n)
+				((m = e[c]),
+				(v = M.style[m]),
+				f(m, '-') && (m = d(m)),
+				M.style[m] !== n)
 			) {
-				if (i || r(o, 'undefined')) return s(), 'pfx' == t ? h : !0
+				if (i || r(o, 'undefined')) return s(), 'pfx' == t ? m : !0
 				try {
-					N.style[h] = o
+					M.style[m] = o
 				} catch (g) {}
-				if (N.style[h] != v) return s(), 'pfx' == t ? h : !0
+				if (M.style[m] != v) return s(), 'pfx' == t ? m : !0
 			}
 		return s(), !1
 	}
-	function v(e, t) {
-		return function() {
-			return e.apply(t, arguments)
-		}
-	}
-	function A(e, t, n) {
-		var o
-		for (var i in e)
-			if (e[i] in t)
-				return n === !1
-					? e[i]
-					: ((o = t[e[i]]), r(o, 'function') ? v(o, n || t) : o)
-		return !1
-	}
 	function g(e, t, n, o, i) {
 		var s = e.charAt(0).toUpperCase() + e.slice(1),
-			a = (e + ' ' + O.join(s + ' ') + s).split(' ')
+			a = (e + ' ' + L.join(s + ' ') + s).split(' ')
 		return r(t, 'string') || r(t, 'undefined')
-			? h(a, t, o, i)
-			: ((a = (e + ' ' + T.join(s + ' ') + s).split(' ')), A(a, t, n))
+			? A(a, t, o, i)
+			: ((a = (e + ' ' + k.join(s + ' ') + s).split(' ')), p(a, t, n))
 	}
 	function y(e, t, r) {
 		return g(e, n, n, t, r)
 	}
 	var C = [],
-		b = {
-			_version: '3.5.0',
+		b = [],
+		w = {
+			_version: '3.6.0',
 			_config: {
 				classPrefix: '',
 				enableClasses: !0,
@@ -228,28 +229,48 @@
 				}, 0)
 			},
 			addTest: function(e, t, n) {
-				C.push({ name: e, fn: t, options: n })
+				b.push({ name: e, fn: t, options: n })
 			},
 			addAsyncTest: function(e) {
-				C.push({ name: null, fn: e })
+				b.push({ name: null, fn: e })
 			}
 		},
 		Modernizr = function() {}
-	;(Modernizr.prototype = b), (Modernizr = new Modernizr())
-	var w = [],
-		S = t.documentElement,
-		x = 'svg' === S.nodeName.toLowerCase(),
-		_ = 'Moz O ms Webkit',
-		T = b._config.usePrefixes ? _.toLowerCase().split(' ') : []
-	b._domPrefixes = T
-	var E = b._config.usePrefixes
+	;(Modernizr.prototype = w),
+		(Modernizr = new Modernizr()),
+		Modernizr.addTest('customelements', 'customElements' in e),
+		Modernizr.addTest('history', function() {
+			var t = navigator.userAgent
+			return (-1 === t.indexOf('Android 2.') &&
+				-1 === t.indexOf('Android 4.0')) ||
+				-1 === t.indexOf('Mobile Safari') ||
+				-1 !== t.indexOf('Chrome') ||
+				-1 !== t.indexOf('Windows Phone') ||
+				'file:' === location.protocol
+				? e.history && 'pushState' in e.history
+				: !1
+		}),
+		Modernizr.addTest('postmessage', 'postMessage' in e)
+	var S = !1
+	try {
+		S = 'WebSocket' in e && 2 === e.WebSocket.CLOSING
+	} catch (x) {}
+	Modernizr.addTest('websockets', S),
+		Modernizr.addTest('picture', 'HTMLPictureElement' in e),
+		Modernizr.addTest('webworkers', 'Worker' in e)
+	var _ = w._config.usePrefixes
 		? ' -webkit- -moz- -o- -ms- '.split(' ')
 		: ['', '']
-	b._prefixes = E
-	var P
+	w._prefixes = _
+	var T = t.documentElement,
+		E = 'svg' === T.nodeName.toLowerCase(),
+		P = 'Moz O ms Webkit',
+		k = w._config.usePrefixes ? P.toLowerCase().split(' ') : []
+	w._domPrefixes = k
+	var O
 	!(function() {
 		var e = {}.hasOwnProperty
-		P =
+		O =
 			r(e, 'undefined') || r(e.call, 'undefined')
 				? function(e, t) {
 						return t in e && r(e.constructor.prototype[t], 'undefined')
@@ -258,8 +279,8 @@
 						return e.call(t, n)
 				  }
 	})(),
-		(b._l = {}),
-		(b.on = function(e, t) {
+		(w._l = {}),
+		(w.on = function(e, t) {
 			this._l[e] || (this._l[e] = []),
 				this._l[e].push(t),
 				Modernizr.hasOwnProperty(e) &&
@@ -267,7 +288,7 @@
 						Modernizr._trigger(e, Modernizr[e])
 					}, 0)
 		}),
-		(b._trigger = function(e, t) {
+		(w._trigger = function(e, t) {
 			if (this._l[e]) {
 				var n = this._l[e]
 				setTimeout(function() {
@@ -278,9 +299,9 @@
 			}
 		}),
 		Modernizr._q.push(function() {
-			b.addTest = s
+			w.addTest = s
 		})
-	var k = (function() {
+	var z = (function() {
 		function e(e, t) {
 			var o
 			return e
@@ -300,8 +321,61 @@
 		var r = !('onblur' in t.documentElement)
 		return e
 	})()
-	b.hasEvent = k
-	var z = (function() {
+	;(w.hasEvent = z),
+		Modernizr.addTest('pointerevents', function() {
+			var e = !1,
+				t = k.length
+			for (e = Modernizr.hasEvent('pointerdown'); t-- && !e; )
+				z(k[t] + 'pointerdown') && (e = !0)
+			return e
+		})
+	var B = function(e, t) {
+		var n = !1,
+			r = a('div'),
+			o = r.style
+		if (e in o) {
+			var i = k.length
+			for (o[e] = t, n = o[e]; i-- && !n; )
+				(o[e] = '-' + k[i] + '-' + t), (n = o[e])
+		}
+		return '' === n && (n = !1), n
+	}
+	;(w.prefixedCSSValue = B),
+		Modernizr.addTest('webgl', function() {
+			var t = a('canvas'),
+				n =
+					'probablySupportsContext' in t
+						? 'probablySupportsContext'
+						: 'supportsContext'
+			return n in t
+				? t[n]('webgl') || t[n]('experimental-webgl')
+				: 'WebGLRenderingContext' in e
+		}),
+		Modernizr.addAsyncTest(function() {
+			var e,
+				t,
+				n,
+				r = a('img'),
+				o = 'sizes' in r
+			!o && 'srcset' in r
+				? ((t =
+						'data:image/gif;base64,R0lGODlhAgABAPAAAP///wAAACH5BAAAAAAALAAAAAACAAEAAAICBAoAOw=='),
+				  (e =
+						'data:image/gif;base64,R0lGODlhAQABAAAAACH5BAEKAAEALAAAAAABAAEAAAICTAEAOw=='),
+				  (n = function() {
+						s('sizes', 2 == r.width)
+				  }),
+				  (r.onload = n),
+				  (r.onerror = n),
+				  r.setAttribute('sizes', '9px'),
+				  (r.srcset = e + ' 1w,' + t + ' 8w'),
+				  (r.src = e))
+				: s('sizes', o)
+		}),
+		Modernizr.addTest('srcset', 'srcset' in a('img'))
+	var L = w._config.usePrefixes ? P.split(' ') : []
+	w._cssomPrefixes = L
+	var R = (function() {
 		var t = e.matchMedia || e.msMatchMedia
 		return t
 			? function(e) {
@@ -326,70 +400,41 @@
 					)
 			  }
 	})()
-	b.mq = z
-	var B = function(e, t) {
-		var n = !1,
-			r = a('div'),
-			o = r.style
-		if (e in o) {
-			var i = T.length
-			for (o[e] = t, n = o[e]; i-- && !n; )
-				(o[e] = '-' + T[i] + '-' + t), (n = o[e])
+	w.mq = R
+	var j = ((w.testStyles = u),
+	function(t) {
+		var r,
+			o = _.length,
+			i = e.CSSRule
+		if ('undefined' == typeof i) return n
+		if (!t) return !1
+		if (
+			((t = t.replace(/^@/, '')),
+			(r = t.replace(/-/g, '_').toUpperCase() + '_RULE'),
+			r in i)
+		)
+			return '@' + t
+		for (var s = 0; o > s; s++) {
+			var a = _[s],
+				l = a.toUpperCase() + '_' + r
+			if (l in i) return '@-' + a.toLowerCase() + '-' + t
 		}
-		return '' === n && (n = !1), n
-	}
-	b.prefixedCSSValue = B
-	var O = b._config.usePrefixes ? _.split(' ') : []
-	b._cssomPrefixes = O
-	var L = { elem: a('modernizr') }
-	Modernizr._q.push(function() {
-		delete L.elem
+		return !1
 	})
-	var N = { style: L.elem.style }
+	w.atRule = j
+	var N = { elem: a('modernizr') }
+	Modernizr._q.push(function() {
+		delete N.elem
+	})
+	var M = { style: N.elem.style }
 	Modernizr._q.unshift(function() {
-		delete N.style
-	}),
-		(b.testAllProps = g),
-		(b.testAllProps = y)
-	;(b.testProp = function(e, t, r) {
-		return h([e], n, t, r)
-	}),
-		(b.testStyles = u)
-	Modernizr.addTest('customelements', 'customElements' in e),
-		Modernizr.addTest('history', function() {
-			var t = navigator.userAgent
-			return (-1 === t.indexOf('Android 2.') &&
-				-1 === t.indexOf('Android 4.0')) ||
-				-1 === t.indexOf('Mobile Safari') ||
-				-1 !== t.indexOf('Chrome') ||
-				-1 !== t.indexOf('Windows Phone') ||
-				'file:' === location.protocol
-				? e.history && 'pushState' in e.history
-				: !1
-		}),
-		Modernizr.addTest('pointerevents', function() {
-			var e = !1,
-				t = T.length
-			for (e = Modernizr.hasEvent('pointerdown'); t-- && !e; )
-				k(T[t] + 'pointerdown') && (e = !0)
-			return e
-		}),
-		Modernizr.addTest('postmessage', 'postMessage' in e),
-		Modernizr.addTest('webgl', function() {
-			var t = a('canvas'),
-				n =
-					'probablySupportsContext' in t
-						? 'probablySupportsContext'
-						: 'supportsContext'
-			return n in t
-				? t[n]('webgl') || t[n]('experimental-webgl')
-				: 'WebGLRenderingContext' in e
-		})
-	var R = !1
-	try {
-		R = 'WebSocket' in e && 2 === e.WebSocket.CLOSING
-	} catch (j) {}
-	Modernizr.addTest('websockets', R),
+		delete M.style
+	})
+	w.testProp = function(e, t, r) {
+		return A([e], n, t, r)
+	}
+	;(w.testAllProps = g),
+		(w.testAllProps = y),
 		Modernizr.addTest('cssanimations', y('animationName', 'a', !0)),
 		(function() {
 			Modernizr.addTest('csscolumns', function() {
@@ -426,35 +471,17 @@
 						(t = t || y(n[r])),
 					Modernizr.addTest('csscolumns.' + e, t)
 		})(),
-		Modernizr.addTest('flexbox', y('flexBasis', '1px', !0)),
-		Modernizr.addTest('picture', 'HTMLPictureElement' in e),
-		Modernizr.addAsyncTest(function() {
-			var e,
-				t,
-				n,
-				r = a('img'),
-				o = 'sizes' in r
-			!o && 'srcset' in r
-				? ((t =
-						'data:image/gif;base64,R0lGODlhAgABAPAAAP///wAAACH5BAAAAAAALAAAAAACAAEAAAICBAoAOw=='),
-				  (e =
-						'data:image/gif;base64,R0lGODlhAQABAAAAACH5BAEKAAEALAAAAAABAAEAAAICTAEAOw=='),
-				  (n = function() {
-						s('sizes', 2 == r.width)
-				  }),
-				  (r.onload = n),
-				  (r.onerror = n),
-				  r.setAttribute('sizes', '9px'),
-				  (r.srcset = e + ' 1w,' + t + ' 8w'),
-				  (r.src = e))
-				: s('sizes', o)
-		}),
-		Modernizr.addTest('srcset', 'srcset' in a('img')),
-		Modernizr.addTest('webworkers', 'Worker' in e),
+		Modernizr.addTest('flexbox', y('flexBasis', '1px', !0))
+	var W = (w.prefixed = function(e, t, n) {
+		return 0 === e.indexOf('@')
+			? j(e)
+			: (-1 != e.indexOf('-') && (e = d(e)), t ? g(e, t, n) : g(e, 'pfx'))
+	})
+	Modernizr.addTest('objectfit', !!W('objectFit'), { aliases: ['object-fit'] }),
 		o(),
-		i(w),
-		delete b.addTest,
-		delete b.addAsyncTest
-	for (var M = 0; M < Modernizr._q.length; M++) Modernizr._q[M]()
+		i(C),
+		delete w.addTest,
+		delete w.addAsyncTest
+	for (var q = 0; q < Modernizr._q.length; q++) Modernizr._q[q]()
 	e.Modernizr = Modernizr
 })(window, document)
